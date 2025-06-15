c903ca4052c9cff155fd808cd80ecdf5adab37f4
Aden Neal <adenocbenji@gmail.com>
2025-06-14 22:59:48 -0500

    models

---
+++ src/components/models/index.jsx
@@ -0,0 +1,44 @@
+/*
+* =================================================================
+* MODELS INDEX
+* FILE: src/components/models/index.jsx
+*
+* This file exports all the individual model components from one
+* convenient place. This makes importing them into your main
+* scene component cleaner.
+* =================================================================
+*/
+export { Couch } from './Couch';
+export { SectionalCouch } from './SectionalCouch';
+export { Chair } from './Chair';
+export { CoffeeTable } from './CoffeeTable';
+export { EndTable } from './EndTable';
+export { TvStand } from './TvStand';
+export { Bookshelf } from './Bookshelf';
+export { Fireplace } from './Fireplace';
+export { KingBed } from './KingBed';
+export { Dresser } from './Dresser';
+export { Nightstand } from './Nightstand';
+export { Desk } from './Desk';
+export { OfficeChair } from './OfficeChair';
+export { FilingCabinet } from './FilingCabinet';
+export { DiningTable } from './DiningTable';
+export { DiningChair } from './DiningChair';
+export { KitchenCounter } from './KitchenCounter';
+export { KitchenIsland } from './KitchenIsland';
+export { Refrigerator } from './Refrigerator';
+export { Stove } from './Stove';
+export { Bathtub } from './Bathtub';
+export { Toilet } from './Toilet';
+export { Rug } from './Rug';
+export { LargeRug } from './LargeRug';
+export { Plant } from './Plant';
+export { TallPlant } from './TallPlant';
+export { Painting } from './Painting';
+export { Mirror } from './Mirror';
+export { FloorLamp } from './FloorLamp';
+export { TableLamp } from './TableLamp';
+export { Wall } from './Wall';
+export { Door } from './Door';
+export { Window } from './Window';
+export { Stairs } from './Stairs';
